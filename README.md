cf902760f36d70b7b51d459304c8ca2b33fb129d
Goldenstriker <ndparab240@gmail.com>
2019-09-24 10:26:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-2lyldg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-2lyldg)
---
+++ index.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+const App = () => {
+  return (<h1>Hello</h1>);
+};
+export default App;
+render(<App />, document.getElementById('root'));
